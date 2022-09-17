05cceb08b39f6c374cb914d7f663051ba4720555
Au9ust <ifeanyi.hillary@gmail.com>
2022-09-17 19:40:44 +0100

    changes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,11 @@
-#ifnder MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
-void postive_or_negative(int i);
-
+void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-#endif /* MAIN_H/
+#endif /* MAIN_H */
