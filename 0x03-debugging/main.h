9138091e37469abfdccdd75eb6831cabab9b363d
Au9ust <ifeanyi.hillary@gmail.com>
2022-09-17 19:02:41 +0100

    new commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,10 @@
-#ifndef MAIN.H
-#define MAIN.H
-
+#ifnder MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+void postive_or_negative(int i);
 
-#endif /* MAIN_H */
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H/
