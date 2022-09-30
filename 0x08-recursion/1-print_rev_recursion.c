4b68bf22b5d25fbad9aeeb9bc8c316332ab4c579
Au9ust <ifeanyi.hillary@gmail.com>
2022-09-30 08:47:11 +0100

    updates to task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,8 +9,8 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s) /*If statement*/
-	}
-		_print_rev_recursions(s + 1); /*add s*/
+	{
+		_print_rev_recursion(s + 1); /*add s*/
 		_putchar(*s); /*print s*/
 	}
 }
